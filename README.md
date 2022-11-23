b578228fd718c17dea172e86d2bed5b5e36d53b8
Florentin Haugwitz <florentin.vonhaugwitz@gmail.com>
2022-11-23 21:56:06 +0000

    Test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NLP_Masterthesis_streamlit
+
 Streamlit app of NLP_Masterthesis project
--- app/app_stt.py
+++ app/app_stt.py
@@ -3,49 +3,9 @@
 Description:
     Implementation of the Streamlit app for the speech-to-text part of the project
 """
-import os
-
 import streamlit as st
-import torch
 from audiorecorder import audiorecorder
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, pipeline  # HubertForCTC,
-
-VOCAB_DIR = os.path.join("models", "stt", "vocab")
-WAV2VEC2_MODEL_DIR = os.path.join("models", "stt", "wav2vec2", "model")
-
-
-def get_device() -> torch.device:
-    """
-    Get torch device
-
-    Returns
-    -------
-    torch.device
-        Torch device
-    """
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def load_trained_model_and_processor_wav2vec2(
-    device: torch.device,
-) -> tuple[Wav2Vec2ForCTC, Wav2Vec2Processor]:
-    """
-    Load the trained model and processor for wav2vec2
-
-    Parameters
-    ----------
-    device : torch.device
-        Torch device
-
-    Returns
-    -------
-    tuple[Wav2Vec2ForCTC, Wav2Vec2Processor]
-        Trained wav2vec2 model and processor
-    """
-    model = Wav2Vec2ForCTC.from_pretrained(WAV2VEC2_MODEL_DIR)
-    processor = Wav2Vec2Processor.from_pretrained(VOCAB_DIR)
-    model.to(device)
-    return model, processor
+from transformers import AutoModelForCTC, AutoProcessor, pipeline
 
 
 def stt_main() -> str | None:
@@ -60,14 +20,15 @@ def stt_main() -> str | None:
     st.title("🎙️ Audio Recorder")
     audio = audiorecorder("Click to record", "Click to stop")
 
-    device = get_device()
-    model, processor = load_trained_model_and_processor_wav2vec2(device)
+    # device = get_device()
+    processor = AutoProcessor.from_pretrained("florentinhaugwitz/wav2vec2_medical")
+    model = AutoModelForCTC.from_pretrained("florentinhaugwitz/wav2vec2_medical")
+
     pipe = pipeline(
         "automatic-speech-recognition",
         model=model,
         tokenizer=processor.tokenizer,
         feature_extractor=processor.feature_extractor,
-        device=0,
     )
 
     if len(audio) > 0:
