fe3adfe5177b267a1537edd372ee33eacb488931
Florentin Haugwitz <florentin.vonhaugwitz@gmail.com>
2022-11-23 11:45:29 +0000

    Updating files

--- app/app.py
+++ app/app.py
@@ -1,3 +1,57 @@
-import streamlit as st
+# -*- coding: utf-8 -*-
+"""
+Description:
+    Implementation of the Streamlit app
 
-st.title("Hello World")
+Usage:
+    $ streamlit run src/streamlit/app.py
+"""
+import streamlit as st
+from src.streamlit.app_clf import clf_main
+from src.streamlit.app_nlp import nlp_main
+from src.streamlit.app_stt import stt_main
+
+import time
+
+# Settings
+st.set_page_config(layout="wide")
+
+# Title
+st.title("Masterthesis Demo")
+st.text("This is a demo of the masterthesis project.")
+
+# Sidebar (Only for demo purposes)
+st.sidebar.title("Debug options")
+debug = st.sidebar.checkbox("Debug mode", value=False)
+
+# Initialization
+if "show_page" not in st.session_state:
+    st.session_state["show_page"] = "stt"
+
+if "res" not in st.session_state:
+    st.session_state["res"] = None
+
+
+# Speech-to-text
+if st.session_state.get("show_page") == "stt":  # type: ignore
+    st.session_state["res"] = stt_main()
+    if st.session_state.get("res"):
+        st.session_state["show_page"] = "nlp"
+        time.sleep(2)
+        st.experimental_rerun()
+
+# NLP
+elif st.session_state.get("show_page") == "nlp":
+    st.session_state["res"] = nlp_main(st.session_state.get("res"))
+    next = st.button("Show next")
+    if next:
+        st.session_state["show_page"] = "clf"
+        st.experimental_rerun()
+
+# CLF
+else:
+    clf_main(st.session_state.get("res"))
+    redo = st.button("Rerun with new input")
+    if redo:
+        st.session_state["show_page"] = "stt"
+        st.experimental_rerun()
---
+++ app/app_nlp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+
+
+def nlp_main(str_nlp: str) -> str:
+    st.write("NLP")
+    st.write(str_nlp)
+    return str_nlp
